9c6c3da74631ed35c249642758e3a511eed34f97
ozikot <37968814+ozikot@users.noreply.github.com>
2018-07-22 13:59:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import TokyoWeather
 import SeleniumMp3
 import GoogleHome
+import pychromecast
+
+### Google Homeをあらかじめ接続しておく ###
+chromecasts = pychromecast.get_chromecasts()
+google_home = chromecasts[0]
 
 ### OpenWeatherMap ###
 message = TokyoWeather.Weather_API()
@@ -11,4 +16,4 @@ url = SeleniumMp3.Texr_Exchange_Mp3(message)
 print(url)
 
 ###Google Home###
-GoogleHome.Play_Mp3(url)
+GoogleHome.Play_Mp3(google_home, url)
